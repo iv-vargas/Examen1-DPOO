98bcc71ac9409bb5443ba3308f2fa371557b711e
iv-vargas <i.vargasr2@uniandes.edu.co>
2026-09-08 09:11:11 -0500

    main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,14 @@
 package Main;
 
 public class Main {
+	public static void main(String[] args) {
+        Hombre hombre1 = new Hombre(70, 160,40);
+        Mujer mujer1 = new Mujer(70, 140,23);
+        Persona ninho1 = new Mujer(30, 120, 15);
+        
+        System.out.println(hombre1.calcularTMB());
+        System.out.println(mujer1.calcularTMB());
+        System.out.println(ninho1.calcularTMB());
+    }
 
 }
